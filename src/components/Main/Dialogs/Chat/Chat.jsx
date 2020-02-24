7430e027a06d0fbb913edd52636e4cc9a809cdaa
JosephDevArt <jbdevart@gmail.com>
2020-02-24 20:44:33 +0300

    finished dialogs section design, start on adding logic

--- src/components/Main/Dialogs/Chat/Chat.jsx
+++ src/components/Main/Dialogs/Chat/Chat.jsx
@@ -12,8 +12,51 @@ function Chat() {
         </div>
       </div>
       <div className="chat_history">
-        <div className="user_message">Hey Man</div>
+        <div className="user_message">
+          Hey Man how is it goint where have you been, to make 2 lines of text
+          bro
+        </div>
         <div className="my_message">Hi Aslan</div>
+        <div className="user_message">
+          Heyave you been, I missed you trying to make 2 lines of text bro
+        </div>
+        <div className="user_message">
+          Hey Man how, I missed you trying t is have you been, , I missed you
+          trying tI missed you trying to make 2 lines of tex, I missed you
+          trying tt bro, I missed you trying t
+        </div>
+        <div className="my_message">
+          {" "}
+          is it goint where have you been, to make 2 lines of text bro
+        </div>
+        <div className="user_message">Hey Man</div>
+        <div className="user_message">
+          Hey Man how is it goint where have you
+        </div>
+        <div className="my_message">Hi Aslan</div>
+        <div className="user_message">Hey Man</div>
+        <div className="user_message">
+          Hey Man how is it goint where have you been, I missed you trying to
+          make 2 lines of text bro
+        </div>
+        <div className="my_message">
+          Hey Mangoint where have you been, I missed you trying to make 2 lines
+          of text bro
+        </div>
+        <div className="user_message">Hey </div>
+        <div className="user_message">
+          Hey Man how is it goi, I missed you trying to make 2 lines of text bro
+        </div>
+        <div className="my_message">Hi Aslan</div>
+        <div className="user_message">Hey Man</div>
+        <div className="user_message">
+          Hey Man how is it goint where have yoyou trying to make 2 lines of
+          text bro
+        </div>
+        <div className="my_message">
+          {" "}
+          is it goint where have you been, to make 2 lines of text bro
+        </div>
         <div className="user_message">Hey Man</div>
       </div>
       <div className="send_message_box">

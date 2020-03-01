9035f381ea9719da3b9a5cec5300a7d214beda81
JosephDevArt <jbdevart@gmail.com>
2020-03-01 17:10:10 +0300

    refractored class components into functional component with hooks (Books.jsx)

--- src/components/Main/Books/Book.jsx
+++ src/components/Main/Books/Book.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 
 function Book(props) {
-  //   console.log(props.items.volumeInfo.title);
-
   return (
     <div className="box">
       <img src={props.items.volumeInfo.imageLinks.thumbnail} alt="" />
--- src/components/Main/Books/Books.jsx
+++ src/components/Main/Books/Books.jsx
@@ -1,102 +1,75 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Books.scss";
 
 import sortIcon from "./iconfinder-icon.svg";
 import Book from "./Book";
-class Books extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: [],
-      totalItems: 0,
-      pagination: 0,
-      isLoaded: false
-    };
-  }
-  componentDidMount() {
+function Books() {
+  const [items, setItems] = useState([]);
+  const [totalItems, setTotalItems] = useState(0);
+  const [pagination, setPagination] = useState(0);
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => {
     fetch(
       `https://www.googleapis.com/books/v1/volumes?q=python&maxResults=5&startIndex=0`,
-      {
-        method: "GET"
-      }
+      { method: "GET" }
     )
       .then(response => response.json())
       .then(data => {
-        this.setState({
-          items: data.items,
-          totalItems: data.totalItems,
-          isLoaded: true,
-          pagination: 5
-        });
-        // console.log(this.state.items[0].volumeInfo.title);
+        setItems(data.items);
+        setTotalItems(data.totalItems);
+        setPagination(5);
+        setLoaded(true);
       })
-      .catch(err => {
-        console.log(err);
-      });
-  }
-  handleClick() {
+      .catch(err => console.log(err));
+  }, []);
+
+  function handleClick(e) {
     fetch(
-      `https://www.googleapis.com/books/v1/volumes?q=python&maxResults=5&startIndex=${this.state.pagination}`,
-      {
-        method: "GET"
-      }
+      `https://www.googleapis.com/books/v1/volumes?q=python&maxResults=5&startIndex=${pagination}`,
+      { method: "GET" }
     )
       .then(response => response.json())
       .then(data => {
-        this.setState(prevState => {
-          prevState.items = [...prevState.items, ...data.items];
-          prevState.pagination = prevState.pagination + 5;
-          // Hey checking merge of branches
-          const hehehey = 123;
-          return prevState;
-        });
+        setItems([...items, ...data.items]);
+        setPagination(pagination + 5);
       });
   }
 
-  render() {
-    console.log(this.state.pagination);
-    if (this.state.isLoaded) {
-      return (
-        <div className="books-section">
-          <button type="button" onClick={this.handleClick.bind(this)}>
-            Click here
-          </button>
-          <div className="total-and-sort">
-            <div className="total-books">
-              <p>Total</p>
-              <h3>{this.state.totalItems} Books</h3>
-            </div>
-            <div className="sort-by">
-              <p>Sort by</p>
-              <div className="sort">
-                <object type="image/sbf+xml" data={sortIcon}>
-                  <img className="icon" src={sortIcon} alt="" />
-                </object>
-
-                <select>
-                  <option value="rate">Rate</option>
-                  <option value="year">Popularity</option>
-                </select>
-              </div>
-            </div>
+  if (loaded) {
+    console.log(items);
+    return (
+      <div className="books-section">
+        <button type="button" onClick={handleClick}>
+          Click here
+        </button>
+        <div className="total-and-sort">
+          <div className="total-books">
+            <p>Total</p>
+            <h3>{totalItems} Books</h3>
           </div>
-          <div className="books-boxes">
-            {this.state.items.map(item => (
-              <Book key={item.id} items={item} />
-            ))}
+          <div className="sort-by">
+            <p>Sort by</p>
+            <div className="sort">
+              <object type="image/sbf+xml" data={sortIcon}>
+                <img className="icon" src={sortIcon} alt="" />
+              </object>
 
-            {/* <Book items={this.state.items[0]} />
-            <Book items={this.state.items[0]} />
-            <Book items={this.state.items[0]} />
-            <Book items={this.state.items[0]} />
-            <Book items={this.state.items[0]} />
-            <Book items={this.state.items[0]} /> */}
+              <select>
+                <option value="rate">Rate</option>
+                <option value="year">Popularity</option>
+              </select>
+            </div>
           </div>
         </div>
-      );
-    } else {
-      return <div>Loading...</div>;
-    }
+        <div className="books-boxes">
+          {items.map(item => (
+            <Book key={item.id} items={item} />
+          ))}
+        </div>
+      </div>
+    );
+  } else {
+    return <div>Loading...</div>;
   }
 }
 

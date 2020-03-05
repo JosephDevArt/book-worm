7b48376e96a69f82a0205c55e1ee0c8f3200394d
JosephDevArt <jbdevart@gmail.com>
2020-03-05 17:28:50 +0300

    refractored code in Books.jsx: optimized sorting functions(less code) + added descending/ascending sorting orders on sorting icon rotation

--- src/components/Main/Books/Books.jsx
+++ src/components/Main/Books/Books.jsx
@@ -8,119 +8,74 @@ function Books() {
   const [items, setItems] = useState([]);
   const [totalItems, setTotalItems] = useState(0);
   const [booksLoaded, setBooksLoaded] = useState(0);
-  const [selectValue, setSelectValue] = useState("pageCount");
+  const [selectValue, setSelectValue] = useState("publishedDate");
+  const [rotateSortIcon, setRotateSortIcon] = useState(false);
   useEffect(() => {
     fetch(
-      `https://www.googleapis.com/books/v1/volumes?q=python&maxResults=5&startIndex=0`,
+      `https://www.googleapis.com/books/v1/volumes?q=python&maxResults=20&startIndex=0`,
       { method: "GET" }
     )
       .then(response => response.json())
       .then(data => {
         setItems(data.items);
         setTotalItems(data.totalItems);
-        setBooksLoaded(5);
+        setBooksLoaded(20);
         setLoaded(true);
       })
       .catch(err => console.log(err));
+    // window.addEventListener("scroll", this.hanldeClick);
   }, []);
+
   useEffect(() => {
     let newItems = items.slice();
-    switch (selectValue) {
-      case "pageCount":
-        newItems
-          .sort(function(a, b) {
-            //---- BELOW ---- Checking for undefind(if 'pageCount' property is missing) => put in the end
-            const pageCountUndefinedA = a.volumeInfo.pageCount == null;
-            const pageCountUndefinedB = b.volumeInfo.pageCount == null;
-            if (pageCountUndefinedA || pageCountUndefinedB) {
-              if (pageCountUndefinedA && pageCountUndefinedB) {
-                return 0;
-              } else if (pageCountUndefinedA) {
-                return -1;
-              } else {
-                return 1;
-              }
-            }
-            //---- ABOVE ---- Checking for undefind(if 'pageCount' property is missing) => put in the end
-            if (a.volumeInfo.pageCount > b.volumeInfo.pageCount) {
-              return 1;
-            } else if (a.volumeInfo.pageCount < b.volumeInfo.pageCount) {
-              return -1;
-            }
-          })
-          .reverse();
-
-        break;
-      case "publishedDate":
-        newItems
-          .sort((a, b) => {
-            //---- BELOW ---- Checking for undefind(if 'publishedDate' property is missing) => put in the end
-            let publishedDateUndefinedA = a.volumeInfo.publishedDate == null;
-            let publishedDateUndefinedB = b.volumeInfo.publishedDate == null;
-            if (publishedDateUndefinedA || publishedDateUndefinedB) {
-              if (publishedDateUndefinedA && publishedDateUndefinedB) {
-                return 0;
-              } else if (publishedDateUndefinedA) {
-                return -1;
-              } else {
-                return 1;
-              }
-            }
-            //---- ABOVE ---- Checking for undefind(if 'publishedDate' property is missing) => put in the end
-            if (
-              parseInt(a.volumeInfo.publishedDate) >
-              parseInt(b.volumeInfo.publishedDate)
-            ) {
-              return 1;
-            } else {
-              return -1;
-            }
-          })
-          .reverse();
-        break;
-      case "rating":
-        newItems
-          .sort((a, b) => {
-            //---- BELOW ---- Checking for undefind(if 'averageRating' property is missing) => put in the end
-            let averageRatingUndefinedA = a.volumeInfo.averageRating == null;
-            let averageRatingUndefinedB = b.volumeInfo.averageRating == null;
-            if (averageRatingUndefinedA || averageRatingUndefinedB) {
-              if (averageRatingUndefinedA && averageRatingUndefinedB) {
-                return 0;
-              } else if (averageRatingUndefinedA) {
-                return -1;
-              } else {
-                return 1;
-              }
-            }
-            //---- ABOVE ---- Checking for undefind(if 'averageRating' property is missing) => put in the end
-            if (a.volumeInfo.averageRating > b.volumeInfo.averageRating) {
-              return 1;
-            } else {
-              return -1;
-            }
-          })
-          .reverse();
-        break;
-      default:
-        newItems = items;
+    newItems.sort(function(a, b) {
+      //---BELOW---Checking for undefined(if 'selectValue' is missing) => put in the end
+      if (
+        a.volumeInfo[selectValue] == null ||
+        b.volumeInfo[selectValue] == null
+      ) {
+        if (
+          a.volumeInfo[selectValue] == null &&
+          b.volumeInfo[selectValue] == null
+        ) {
+          return 0;
+        } else if (a.volumeInfo[selectValue] == null) {
+          return 1;
+        } else {
+          return -1;
+        }
+      }
+      //---ABOVE---Checking for undefined(if 'selectValue' is missing) => put in the end
+      if (a.volumeInfo[selectValue] < b.volumeInfo[selectValue]) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+    // Sort by Descending/Ascending order
+    if (rotateSortIcon) {
+      newItems.reverse();
     }
     setItems(newItems);
-  }, [selectValue, items.length]);
+  }, [selectValue, items.length, rotateSortIcon]);
 
   function handleClick(e) {
     fetch(
-      `https://www.googleapis.com/books/v1/volumes?q=python&maxResults=5&startIndex=${booksLoaded}`,
+      `https://www.googleapis.com/books/v1/volumes?q=python&maxResults=20&startIndex=${booksLoaded}`,
       { method: "GET" }
     )
       .then(response => response.json())
       .then(data => {
-        setBooksLoaded(booksLoaded + 5);
+        setBooksLoaded(booksLoaded + 20);
         setItems([...items, ...data.items]);
       });
   }
   const handleChange = e => {
     setSelectValue(e.target.value);
+  };
+
+  const imageClick = () => {
+    setRotateSortIcon(!rotateSortIcon);
   };
 
   if (loaded) {
@@ -139,14 +94,19 @@ function Books() {
             <p className="sort-text">Sort by</p>
             <div className="sort-boxes">
               <object type="image/sbf+xml" data={sortIcon}>
-                <img className="icon-sort" src={sortIcon} alt="" />
+                <img
+                  onClick={imageClick}
+                  className={`icon-sort + ${rotateSortIcon ? "rotate" : ""}`}
+                  src={sortIcon}
+                  alt=""
+                />
               </object>
               <select
                 value={selectValue}
                 className="sort-select"
                 onChange={handleChange}
               >
-                <option value="rating">Rating</option>
+                <option value="averageRating">Rating</option>
                 <option value="pageCount">Page Count</option>
                 <option value="publishedDate">Published date</option>
               </select>
